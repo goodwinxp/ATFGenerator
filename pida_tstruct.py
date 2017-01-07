cd3eb821f7c203cba2ac740439103e9f16b9f637
goodwin <b1ll.cipher@yandex.ru>
2017-01-07 22:59:16 +0300

    Add struct decoding

---
+++ pida_tstruct.py
@@ -0,0 +1,21 @@
+from pida_abc_type import IdaTypes
+from pida_types import IDA_TYPES
+from pida_type_decoder import decode_hybrid_type
+
+
+class IdaTStruct(IdaTypes):
+    def __init__(self, ida_type=IDA_TYPES['struct']):
+        self.ida_type = {'idt': ida_type, 'value': []}
+
+    def decode(self, data):
+        count = ord(data[0])
+        offset = 1
+        for i in range(0, count):
+            len, value = decode_hybrid_type(ida_type=data[offset:])
+            offset += len
+            self.ida_type['value'].append(value)
+
+        return offset
+
+    def get_type(self):
+        return self.ida_type
