97d831e50a3366c6886bab97258e169646cd1a60
goodwin <b1ll.cipher@yandex.ru>
2017-07-10 02:55:39 +0300

    Replaced tab to space

--- ATFGen/common/common.h
+++ ATFGen/common/common.h
@@ -12,23 +12,23 @@
 #define END_ATF_NAMESPACE };
 
 START_ATF_NAMESPACE
-	#include "ATFRegister.hpp"
-	
-	const uint16_t usVersion = 0x0100;
-	const wchar_t wszVersion[] = L"1.0";
-	
-	typedef struct _hook_record {
-		LPVOID pTrgAppOrig;	// Адрес в целевом приложении
-		LPVOID* ppOrig;		// clbk
-		LPVOID* ppTramp;	// tramp
-		LPVOID  pWrapper;	// tramp
-		LPVOID  pBind;		// bind
-	} hook_record;
-	
-	template<typename _Ty, size_t _Need, size_t _Have = sizeof(_Ty)>
-	constexpr bool checkSize()
-	{
-		static_assert(_Have == _Need, "Invalid size");
-		return true;
-	}
+    #include "ATFRegister.hpp"
+    
+    const uint16_t usVersion = 0x0100;
+    const wchar_t wszVersion[] = L"1.0";
+    
+    typedef struct _hook_record {
+        LPVOID pTrgAppOrig; // Адрес в целевом приложении
+        LPVOID* ppOrig;     // clbk
+        LPVOID* ppTramp;    // tramp
+        LPVOID  pWrapper;   // tramp
+        LPVOID  pBind;      // bind
+    } hook_record;
+    
+    template<typename _Ty, size_t _Need, size_t _Have = sizeof(_Ty)>
+    constexpr bool checkSize()
+    {
+        static_assert(_Have == _Need, "Invalid size");
+        return true;
+    }
 END_ATF_NAMESPACE
